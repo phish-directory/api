5dc92e8422db36f1d1f6d6a2900c2e9d37df0351
Jasper Mayone <me@jaspermayone.com>
2024-07-10 01:29:56 -0400

    wip

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,11 +1,34 @@
 import Vapor
 
 func routes(_ app: Application) throws {
-    app.get { req async in
-        "It works!"
+    app.get { req async throws in
+        "It works! Check out the api docs if you need docs"
     }
 
-    app.get("hello") { req async -> String in
-        "Hello, world!"
+    app.get("health") { req async throws in
+        "OK"
+    }
+
+    app.get("check") { req async throws in
+
+    let urls = [
+        "google.com",
+        "apple.com",
+    ]
+
+    // check if the req.body contains the key "url"
+    guard let url = req.query[String.self, at: "url"] else {
+        throw Abort(.badRequest)
+    }
+
+    // check if the url is in the urls array, and return if it is
+    if urls.contains(url) {
+        return "URL is in the list"
+
+    } else {
+        return "URL is not in the list"
+    }
+
+
     }
 }
