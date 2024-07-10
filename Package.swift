c7c882d879b6495d1b1807e6fc1796cee19e991c
Jasper Mayone <me@jaspermayone.com>
2024-07-10 01:05:50 -0400

    packagessssss

--- Package.swift
+++ Package.swift
@@ -11,6 +11,12 @@ let package = Package(
         .package(url: "https://github.com/vapor/vapor.git", from: "4.99.3"),
         // 🔵 Non-blocking, event-driven networking for Swift. Used for custom executors
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.65.0"),
+        // 🐯 OpenAPI runtime & generator for Swift
+        .package(url: "https://github.com/apple/swift-openapi-runtime", from: "1.4.0"),
+        .package(url: "https://github.com/apple/swift-openapi-generator", from: "1.2.1"),
+        // vapor openapi
+        .package(url: "https://github.com/swift-server/swift-openapi-vapor", from: "1.0.1")
+
     ],
     targets: [
         .executableTarget(
