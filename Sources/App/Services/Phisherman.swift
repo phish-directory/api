fcf995ae15b71ff540d074868086ea1d5ba8fd7f
Jasper Mayone <me@jaspermayone.com>
2024-07-13 17:00:05 -0400

    new identity plus phisherman

--- Sources/App/Services/Phisherman.swift
+++ Sources/App/Services/Phisherman.swift
@@ -1,7 +1,7 @@
 import Foundation
 import Vapor
 
-struct IpQualityScore {
+struct Phisherman {
 
 // check function that returns data
   func check(domain: String) async -> {
@@ -10,9 +10,10 @@ struct IpQualityScore {
 
     let domain = domain.canonicalizeURL()
 
-    let apiKey = ProcessInfo.processInfo.environment["IPQS_API_KEY"]
+    let apiKey = ProcessInfo.processInfo.environment["PHISHERMAN_API_KEY"]
+
     let url = URL(
-      string: "https://www.ipqualityscore.com/api/json/url/\(apiKey)/\(domain)")!
+      string: "https://api.phisherman.gg/v2/domains/check/\(domain)")!
 
     var request = URLRequest(url: url)
     request.httpMethod = "GET"
@@ -21,12 +22,14 @@ struct IpQualityScore {
     request.setValue("application/json", forHTTPHeaderField: "Content-Type")
     request.setValue("https://phish.directory", forHTTPHeaderField: "Referer")
 
+    // add bearer token
+    request.setValue("Bearer \(apiKey)", forHTTPHeaderField: "Authorization")
+
     // make the request
     let (data, _) = try await URLSession.shared.data(for: request)
 
-    let response = try JSONDecoder().decode(IpQualityScoreResponse.self, from: data)
+    let response = try JSONDecoder().decode(PhishermanResponse.self, from: data)
 
     return response
   }
-
 }
