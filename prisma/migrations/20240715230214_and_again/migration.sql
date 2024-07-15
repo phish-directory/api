6be975098e46f82bcc821c4a6cce933aa560ea30
Jasper Mayone <jasper.mayone@phish.directory>
2024-07-15 19:05:37 -0400

    more work

---
+++ prisma/migrations/20240715230214_and_again/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `region` on the `IpQualityScoreAPIResponse` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "IpQualityScoreAPIResponse" DROP COLUMN "region";
