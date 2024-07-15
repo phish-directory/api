fabb916b76db28cc09e85efcf1e143057e04697f
Jasper Mayone <me@jaspermayone.com>
2024-07-15 19:00:49 -0400

    more work to try and debug

---
+++ prisma/migrations/20240715225849_bugs/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `country` on the `IpQualityScoreAPIResponse` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "IpQualityScoreAPIResponse" DROP COLUMN "country";
