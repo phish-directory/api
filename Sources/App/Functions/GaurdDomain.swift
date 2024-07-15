7cb3b903108936faee3e8f4144538444735be0f4
Jasper Mayone <jasper.mayone@phish.directory>
2024-07-15 09:13:58 -0400

    more moves :

--- Sources/App/Functions/GaurdDomain.swift
+++
@@ -1,6 +0,0 @@
-// create a function that checks if a string is null or empty, and throws an error if it is
-func guardDomain(_ domain: String) throws {
-  guard !domain.isEmpty else {
-    throw Abort(.badRequest, reason: "Missing Domain")
-  }
-}
