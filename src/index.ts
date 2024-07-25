ec09130081d65b17610a2d0eaa94f400ce2f4ac0
Jasper Mayone <me@jaspermayone.com>
2024-07-25 17:57:21 -0400

    todo

--- src/index.ts
+++ src/index.ts
@@ -34,6 +34,7 @@ app.use("/", router);
 - https://urlhaus-api.abuse.ch/
 - https://openphish.com/
 - https://report.netcraft.com/api/v3#tag/Report/paths/~1report~1mistake/post
+- https://any.run/api-documentation/
 */
 console.log(new Date().getTime());
 
