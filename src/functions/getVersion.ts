05adf04d90d828ea3585d0bd23d14ac4aa5d069e
Jasper Mayone <jasper.mayone@phish.directory>
2024-08-16 16:48:46 -0400

    new system for package versions

--- src/functions/getVersion.ts
+++ src/functions/getVersion.ts
@@ -2,3 +2,8 @@ export function getVersion() {
   const packageJson = require("../../package.json");
   return packageJson.version;
 }
+
+export function getPackageVersion(packageName: string) {
+  const packageJson = require("../../package.json");
+  return packageJson.dependencies[packageName];
+}
